fb3416ac7904213a9d9ea274699022e47a9606cd
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-10-14 14:02:45 +0900

    refacor: 불필요한 파일 삭제 | feat: Get API by Postman

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
+import axios from "axios";
+
 function App() {
+  axios({
+    method: "get",
+    url: "https://c184e4c2-f4cd-452f-a31f-5a72f2d5076c.mock.pstmn.io/trip",
+    responseType: "json",
+  }).then(function (response) {
+    console.log(response.data);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
