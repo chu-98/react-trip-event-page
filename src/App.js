fcbc8f34e8c4c5631cf0a114f5d0ca52b2e980d7
chu-98 <81007078+chu-98@users.noreply.github.com>
2021-11-18 21:31:48 +0900

    Update

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 function App() {
-  axios({
-    method: 'get',
-    url: 'https://69cee40f-f372-4734-a2ff-a043da89d0b2.mock.pstmn.io/location',
+  // axios({
+  //   method: 'get',
+  //   url: 'https://69cee40f-f372-4734-a2ff-a043da89d0b2.mock.pstmn.io/location',
 
-    responseType: 'json',
-  }).then(function (response) {
-    console.log(response.data);
-  });
+  //   responseType: 'json',
+  // }).then(function (response) {
+  //   console.log(response.data);
+  // });
 
-  axios({
-    method: 'get',
-    url: 'https://69cee40f-f372-4734-a2ff-a043da89d0b2.mock.pstmn.io/activity',
-
-    responseType: 'json',
-  }).then(function (response) {
-    console.log(response.data);
-  });
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const Door = styled.return(<Door></Door>);
 }
 
 export default App;
